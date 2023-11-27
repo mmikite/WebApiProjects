0dcbb3ff4012caf1b5d87d248d3dfe97df4cf43a
Márton Mikite <marton.mikite@stud.uni-corvinus.hu>
2023-11-27 14:17:12 +0100

    elkeszult a vicces probafeladat

--- WebAPIBase/wwwroot/scripts/geek.js
+++ WebAPIBase/wwwroot/scripts/geek.js
@@ -1,1 +1,24 @@
-﻿
+﻿var viccek;
+
+window.onload = _ => letoltes()
+
+function letoltes() {
+	fetch('/jokes.json')
+		.then(response => response.json())
+		.then(data => letoltesBef(data)
+		);
+}
+
+function letoltesBef(d) {
+	console.log("Sikeres letöltés")
+	console.log(d)
+	let ide = document.getElementById("ide");
+	viccek = d;
+
+	for (var i = 0; i < viccek.length; i++) {
+		console.log(viccek[i].text)
+		let elem = document.createElement("li")
+		elem.innerHTML = viccek[i].text
+		ide.appendChild(elem)
+	}
+}
