4a483e59bd0231d739da2f7cff0c1927d20ebeba
Márton Mikite <marton.mikite@stud.uni-corvinus.hu>
2023-12-04 14:57:10 +0100

    mukodik id alapjan akerdes

--- Hajos/wwwroot/script/HajosKerdes.js
+++ Hajos/wwwroot/script/HajosKerdes.js
@@ -1,11 +1,18 @@
-﻿window.onload = letoltes
+﻿let kerdesszam = 6
+window.onload = kérdésBetöltés(kerdesszam)
 
-function letoltes() {
-    fetch('/questions/16')
-        .then(response => response.json())
-        .then(data => kérdésMegjelenítés(data)
-        );
-}
+function kérdésBetöltés(id) {
+    fetch(`/questions/${id}`)
+        .then(response => {
+            if (!response.ok) {
+                console.error(`Hibás válasz: ${response.status}`)
+            }
+            else {
+                return response.json()
+            }
+        })
+        .then(data => kérdésMegjelenítés(data));
+}    
 
 function kérdésMegjelenítés(kérdés) {
     console.log(kérdés);
@@ -14,4 +21,6 @@ function kérdésMegjelenítés(kérdés) {
     document.getElementById("válasz2").innerText = kérdés.answer2
     document.getElementById("válasz3").innerText = kérdés.answer3
     document.getElementById("kép").src = "https://szoft1.comeback.hu/hajo/" + kérdés.image;
+
 }
+
